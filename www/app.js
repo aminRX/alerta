4dc6e75d40ca8b9ae1ba6a941beb08f5d2282905
Amin Ogarrio <amin.ogarrio@gmail.com>
2015-11-17 17:18:29 -0700

    New Report views.

--- www/app.js
+++ www/app.js
@@ -1,37 +1,56 @@
 angular.module('alerta', ['ionic', 'alerta.Geolocation'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      StatusBar.styleLightContent();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  $stateProvider
-    .state('tab', {
-      url: '/tab',
-      abstract: true,
-      templateUrl: 'tabs/tabs.html'
-    })
-    .state('tab.geolocation', {
-      url: '/geolocation',
-      views: {
-        'tab-geolocation-show': {
-          controller: 'GeolocationShowController as geo',
-          templateUrl: 'geolocation/show.html'
-        }
-      },
-      data: {
+      }
+      if (window.StatusBar) {
+        StatusBar.styleLightContent();
       }
     });
+  })
+
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'tabs/tabs.html'
+      })
+
+      .state('tab.geolocation', {
+        url: '/geolocation',
+        views: {
+          'tab-geolocation-show': {
+            controller: 'GeolocationShowController as geo',
+            templateUrl: 'geolocation/show.html'
+          }
+        },
+        data: {
+        }
+      })
+
+      .state('report', {
+        url: '/report',
+        abstract: true,
+        templateUrl: 'report/report.html'
+      })
+
+      .state('report.details', {
+        url: '/details',
+        views: {
+          'report-details': {
+            controller: 'ReportDetailsController as detail',
+            templateUrl: 'report/details.html'
+          }
+        }
+      });
+
+
 
   $urlRouterProvider.otherwise('/tab/geolocation');
 
